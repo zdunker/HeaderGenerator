07e0d260801bd6e93dea0cb1cb5434bc9d5b4acf
zdunker <woshilisheng@gmail.com>
2016-09-06 15:27:52 -0700

    v1 modifications

--- Main.py
+++ Main.py
@@ -10,9 +10,11 @@ import time
 
 slnFileSyntax = re.compile(r'''Project(\(\S+\)) = \"(.*?)\", \"(.*?)\"''')#group3 contains location
 projectSourceFileSyntax = re.compile(r'''<ClCompile Include="(.*?)"''')#group1 contains location
-includeSyntax = re.compile(r'''^#include [<|"](.*?)[>|"]''')#group1 contains location
+includeSyntax = re.compile(r'''^#include (\S+)''')#group1 contains location
 sourceSuffixes = (".c",".cc",".cpp")
+standardHeaderClosings = ('"','>')
 
+# return a list of projects from reading solution file
 def getProjects(solutionFile):
     projectList = []
     solutionDir = os.path.dirname(solutionFile)
@@ -25,14 +27,15 @@ def getProjects(solutionFile):
         slnContent = rf.readlines()
     for line in slnContent:
         if "Project(" in line:
-            mo = slnFileSyntax.search(line)
-            if os.path.exists(os.path.join(solutionDir,mo.group(3))):
-                projectList.append(os.path.normpath(os.path.join(solutionDir,mo.group(3))))
+            matchedObject = slnFileSyntax.search(line)
+            if os.path.exists(os.path.join(solutionDir,matchedObject.group(3))):
+                projectList.append(os.path.normpath(os.path.join(solutionDir,matchedObject.group(3))))
             else:
-                print "Notice: %s can not be found"%(os.path.join(solutionDir,mo.group(3)))
+                print "Notice: %s can not be found"%(os.path.join(solutionDir,matchedObject.group(3)))
                 
     return projectList
 
+#return a list of source files from reading project file
 def getFiles(projectFile):
     sourceList = []
     projectDir = os.path.dirname(projectFile)
@@ -45,20 +48,23 @@ def getFiles(projectFile):
         projectContent = rf.readlines()
     for line in projectContent:
         if "<ClCompile Include=" in line:
-            mo = projectSourceFileSyntax.search(line)
-            if os.path.exists(os.path.join(projectDir,mo.group(1))):
-                sourceList.append(os.path.normpath(os.path.join(projectDir,mo.group(1))))
+            matchedObject = projectSourceFileSyntax.search(line)
+            if os.path.exists(os.path.join(projectDir,matchedObject.group(1))):
+                sourceList.append(os.path.normpath(os.path.join(projectDir,matchedObject.group(1))))
             else:
-                print "Notice: %s can not be found"%(os.path.join(projectDir,mo.group(1)))
+                print "Notice: %s can not be found"%(os.path.join(projectDir,matchedObject.group(1)))
                 
     return sourceList
 
+# determine user-defined location is a folder or a certain type of file
 def determineLocation(location, suffix=''):
     if not suffix:
         return os.path.isdir(location)
     else:
         return location.endswith(suffix)
 
+# this function executes only if user threw in a folder, it's hard to tell which source to compile
+# so just push every source file to list, suggestions are welcome
 def getSourceList(fileLocation):
     sourceList = []
     for root, dirs, files in os.walk(fileLocation):
@@ -67,42 +73,46 @@ def getSourceList(fileLocation):
                 sourceList.append(os.path.join(root,file))
     return sourceList
 
+# return a list of headers
 def getHeaderList(sourceList, root):
     headerList = []
     for source in sourceList:
-        headerList += getHeaders(source, root)
+        getHeaders(source, root, headerList)
     return list(set(headerList))
 
+# this function is an extra step for solution file only, return a list of projects
 def getProjectsSources(projectList):
     sourcelist = []
     for project in projectList:
         sourcelist += getFiles(project)
     return list(set(sourcelist))
 
-#To use this recursive function: source you are passing in should be full path
-#                                and the location you throw in should be the root directory
-#                                a.k.a. the search interval this function will scan in
-#                                I do not recommand general location like 'C:\'
-def getHeaders(source, location, headerList=[]):
-    inputHeaderListLen = len(headerList)
+# read source file and use what's after #include to find headers
+# check depth 1 and 2 of referenced headers, and push distinct headers into list
+def getHeaders(source, location, headerList=[], level = 0):
+    level += 1
     with open(source) as rf:
-        sourceContent = rf.readlines()
+        sourceContent  = rf.readlines()
     for line in sourceContent:
-        mo  = includeSyntax.search(line)
-        if mo:
+        mo = includeSyntax.search(line)
+        if mo and (mo.group(1))[-1] in standardHeaderClosings:
+            # clean up mo.group(1)
+            targetHeader = mo.group(1)[1:len(mo.group(1))-1]
+            # standard ratio
             for root, dirs, files in os.walk(location):
                 for file in files:
-                    if (os.path.normpath(mo.group(1)) in os.path.join(root, file) and 
+                    if (os.path.normpath(targetHeader) in os.path.join(root, file) and
                         not os.path.join(root, file) in headerList):
                         print "Appending : %s"%(os.path.join(root, file))
                         headerList.append(os.path.join(root, file))
-    modifiedHeaderListLen = len(headerList)
-    if modifiedHeaderListLen - inputHeaderListLen > 0:
-        for i in range(len(headerList)):
-            return getHeaders(headerList[i],location, headerList)
-    elif modifiedHeaderListLen - inputHeaderListLen == 0:
-        return headerList
-
+                        # end on depth 2
+                        if level < 2:
+                            getHeaders(os.path.join(root, file), location, headerList, level)
+        elif mo and not (mo.group(1))[-1] in standardHeaderClosings:
+            # macro ratio
+            # developing
+            print "%s might be a macro..."%(mo.group(1))
+            
 def copyHeaders(headerList, root, copyDestination):
     for header in headerList:
         toWhere = os.path.join(copyDestination, os.path.relpath(header, root))
@@ -128,6 +138,16 @@ def projectOperation(fileLocation, root, copyDestination):
     headerList = getHeaderList(sourceList, root)
     copyHeaders(headerList, root, copyDestination)
 
+def errorCommandProtector(sleepSeconds):
+    while sleepSeconds > 0:    
+        print "If not as expected, please interrupt in %d seconds"%(sleepSeconds)
+        sleepSeconds -= 1
+        sleep(1)
+
+# three arguments are required, location is the input folder/file
+#                               root is the directory tells automation where to start looking for headers
+#                               destination is the directory where headers are copying to
+# root argument is an ugly one, any suggestion can help get rid of it is welcome
 def getArgs():
     parser = argparse.ArgumentParser(prog="HeaderGenerator", usage="%(prog)s [options]",
                                     description="Generate header files from input location/file")
@@ -138,11 +158,14 @@ def getArgs():
     args = parser.parse_args()
     
     if not args.location or not args.root:
-        print 'Invalid input'
+        print "Invalid input"
         exit(-1)
     
     if args.destination:
-        print 'Will copy headers to {}'.format(args.destination)
+        print "Will copy headers to %s"%(args.destination)
+    if not args.destination:
+        print "Will copy headers to %s\\testInclude"%(args.location)
+        errorCommandProtector(5)
     
     return args.location, args.root, args.destination
 
@@ -160,7 +183,7 @@ def main(fileLocation, root, copyDestination):
         projectOperation(fileLocation, root, copyDestination)
         
     else:
-        print "%s does not fit in any scenario, exit"%(fileLocation)
+        print "%s file type not supported, exit"%(fileLocation)
         exit(-1)
 
 if __name__ == '__main__':
